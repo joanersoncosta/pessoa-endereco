566777725819c23fdac258830561a7d2ed9f0150
Joanerson Costa <joanersonc31@gmail.com>
2023-10-23 09:07:13 -0300

    implementa api de teste

---
+++ src/main/java/br/com/attornatus/PessoaEnderecoApplication.java
@@ -0,0 +1,23 @@
+package br.com.attornatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class PessoaEnderecoApplication {
+
+	@GetMapping(value = "home")
+	public String getHomeText() {
+		return "Pessoa Endereco - API Home";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaEnderecoApplication.class, args);
+	}
+
+}
