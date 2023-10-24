466baba92a176d09749687fa40940bbd6fd6a3d5
Joanerson Costa <joanersonc31@gmail.com>
2023-10-24 15:46:05 -0300

    implementa tratamento de erro para excecao duplicada

---
+++ src/main/java/br/com/attornatus/pessoa/application/api/PessoaAlteracaoRequest.java
@@ -0,0 +1,16 @@
+package br.com.attornatus.pessoa.application.api;
+
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
+public class PessoaAlteracaoRequest {
+	
+	@NotBlank
+	private String nomeCompleto;
+	@NotNull
+	private LocalDate dataNascimento;
+}
