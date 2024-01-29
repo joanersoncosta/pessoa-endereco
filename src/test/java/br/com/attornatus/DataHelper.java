11146e304b5160487b147448a15933ccc4f27ae7
Joanerson Costa <joanersonc31@gmail.com>
2024-01-29 17:37:42 -0300

    implementa teste busca listPessoas

--- src/test/java/br/com/attornatus/DataHelper.java
+++ src/test/java/br/com/attornatus/DataHelper.java
@@ -2,19 +2,28 @@ package br.com.attornatus;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import br.com.attornatus.pessoa.application.api.PessoaRequest;
 import br.com.attornatus.pessoa.domain.Pessoa;
 
 public class DataHelper {
-	private static final UUID pessoa1 = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
+	private static final UUID ID_PESSOA1 = UUID.fromString("a713162f-20a9-4db9-a85b-90cd51ab18f4");
 
 	public static Pessoa createPessoa() {
-		return Pessoa.builder().idPessoa(pessoa1).nome("pessoa 1").dataNascimento(LocalDate.parse("1998-05-14")).momentoDoDacastro(LocalDateTime.now()).build();
+		return Pessoa.builder().idPessoa(ID_PESSOA1).nome("pessoa 1").dataNascimento(LocalDate.parse("1998-05-14")).momentoDoDacastro(LocalDateTime.now()).build();
 	}
 
 	public static PessoaRequest getPessoaRequest() {
 		return new PessoaRequest("pessoa 1", LocalDate.parse("1998-05-14"));
+	}
+	
+	public static List<Pessoa> getListPessoa() {
+		return List.of(
+				Pessoa.builder().idPessoa(UUID.randomUUID()).nome("Pessoa 1").dataNascimento(LocalDate.parse("1998-05-14")).momentoDoDacastro(LocalDateTime.now()).dataHoraDaultimaAlteracao(null).build(),
+				Pessoa.builder().idPessoa(UUID.randomUUID()).nome("Pessoa 2").dataNascimento(LocalDate.parse("2002-02-22")).momentoDoDacastro(LocalDateTime.now()).dataHoraDaultimaAlteracao(null).build(),
+				Pessoa.builder().idPessoa(UUID.randomUUID()).nome("Pessoa 3").dataNascimento(LocalDate.parse("2003-03-03")).momentoDoDacastro(LocalDateTime.now()).dataHoraDaultimaAlteracao(null).build(),
+				Pessoa.builder().idPessoa(UUID.randomUUID()).nome("Pessoa 4").dataNascimento(LocalDate.parse("2004-04-15")).momentoDoDacastro(LocalDateTime.now()).dataHoraDaultimaAlteracao(null).build());
 	}
 }
--- src/test/java/br/com/attornatus/pessoa/application/service/PessoaApplicationServiceTest.java
+++ src/test/java/br/com/attornatus/pessoa/application/service/PessoaApplicationServiceTest.java
@@ -1,9 +1,14 @@
 package br.com.attornatus.pessoa.application.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +18,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.attornatus.DataHelper;
 import br.com.attornatus.pessoa.application.api.PessoaIdResponse;
+import br.com.attornatus.pessoa.application.api.PessoaListResponse;
 import br.com.attornatus.pessoa.application.api.PessoaRequest;
 import br.com.attornatus.pessoa.application.repository.PessoaRepository;
 import br.com.attornatus.pessoa.domain.Pessoa;
@@ -33,6 +39,22 @@ class PessoaApplicationServiceTest {
 		
 		assertNotNull(response);
 		assertEquals(PessoaIdResponse.class, response.getClass());
+	}
+	
+	@Test
+	void testbuscaListPessoa() {
+		
+		List<Pessoa> request = DataHelper.getListPessoa();
+		
+		when(pessoaRepository.buscaTodasPessoas()).thenReturn(request);
+		List<PessoaListResponse> response = pessoaApplicationService.buscaTodasPessoas();
+		
+		verify(pessoaRepository, times(1)).buscaTodasPessoas();
+		
+//		assertNotNull(response);
+//		assertEquals(response, 4);
+		assertThat(response).isNotEmpty();
+		assertThat(response).hasSize(4);
 	}
 
 }
